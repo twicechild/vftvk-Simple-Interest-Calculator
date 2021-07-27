5a58856d25d39af18575c2c9762c92bb7ff01434
twicechild <blacknas@grannysparlor.gr>
2021-07-27 18:20:26 +0300

    TEST

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 function compute()
 {
     
+    
+
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year =  new Date().getFullYear()+parseInt(years);
 
+    if(principal > 0)
+    {
     document.getElementById("result").innerHTML="\<br>\If you deposit <strong>"+principal+"</strong>,\<br\>at an interest rate of <strong>"+rate+"%</strong>\<br\>You will receive an amount of <strong>"+parseInt(interest)+"</strong>,\<br\>in the year <strong>"+year+"</strong>\<br\>";
+    }else{
+        
+        alert("Please enter an amount that is more than zero");
+        document.getElementById("principal").focus();
+    
+    }
 
 }
 
@@ -15,15 +25,4 @@ function updateRate()
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
-}
-
-function validateAmount()
-{
-    var amount = document.getElementById(principal)
-    var isPositive = amount > 0;
-    if(!isPositive)
-    {
-        alert("Please enter an amount that is more than zero (0)")
-        document.getElementById("principal").focus();
-    }
 }
